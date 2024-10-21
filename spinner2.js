fd956166194b01264cdc5d4502931754d3046e7b
Nik Hixson <nikilobe@gmail.com>
2024-10-20 20:07:27 -0700

    Trying adding all of the spinner icons into an array and then iterating through them. Just need to figure out how to incorporate \r

--- spinner2.js
+++ spinner2.js
@@ -1,35 +1,43 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
 
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 700);
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 700);
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 900);
-//added additional blocks of code below to complete animation spin
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 1100);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1300);
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1100);
 
-setTimeout(() => {
-  process.stdout.write('\r\\   ');
-}, 1500);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1700);
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 1700);
+
+const spinner = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+
+for (let i in spinner) {
+  setTimeout(() => {
+    process.stdout.write(spinner[i]);
+  }, i * 200);
+}
